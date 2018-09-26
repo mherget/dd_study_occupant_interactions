eff2bf8538d5742fda4c0bdb0311f5919116b3f3
mherget <mhchris@elektro.dtu.dk>
2018-09-26 12:05:10 +0200

    Fixed relative path to metadatafile

--- metadata_import.R
+++ metadata_import.R
@@ -1,10 +1,13 @@
 rm(list=ls())
 
 apt_no <- "A31"
+#metadata file location relative
+dir <- "./Sundmolen meta data file correct version/"
 #df <- read.csv (paste("2018_",i,"_",sensor$code[j],"_data.csv",sep=""))
 
+
 #load metadata into dataframe from csv file
-df <- read.csv (paste(apt, "meta data fil.csv"), sep =";", na.strings=c("", "NA"))
+df <- read.csv (paste(paste(dir,apt_no, sep = ""), "meta data fil.csv"), sep =";", na.strings=c("", "NA"))
 df_clean <- na.omit(df)
 
 # Include only those where the unit is given i.e. we are dealing with a measurement / data point
@@ -18,5 +21,5 @@ sensor = data.frame("code" = df_clean$adr,
 
 #subsets can be selected, e.g. based on unit or address space
 sensor[sensor$unit == "degC" ,]
-sensor[sensor$code == "" ,]
+
 
